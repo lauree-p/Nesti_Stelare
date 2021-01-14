f376eca609f378dbad73f04c81a9537428fa7972
ste <s.cadeck@gmail.com>
2021-01-14 17:42:55 +0100

    add view_login

---
+++ Nesti_Stelare/src/views/View_Login.java
@@ -0,0 +1,93 @@
+package views;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class View_Login {
+
+	private static final String CENTER = null;
+	private JFrame frame;
+	private JTextField name;
+	private JTextField passWord;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					View_Login window = new View_Login();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public View_Login() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(46, 22, 14));
+		frame.getContentPane().setLayout(null);
+		frame.setBounds(100, 100, 575, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLabel img = new JLabel(new ImageIcon(View_Login.class.getResource("/img/connect-image.png")));
+		img.setBounds(42, 0, 478, 127);
+		frame.getContentPane().add(img);
+
+		JLabel name_user = new JLabel("Nom utilisateur");
+		name_user.setForeground(new Color(230, 167, 86));
+		name_user.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		name_user.setHorizontalAlignment(SwingConstants.CENTER);
+		name_user.setBounds(21, 170, 157, 14);
+		frame.getContentPane().add(name_user);
+
+		JLabel passWord_user = new JLabel("Mot de passe");
+		passWord_user.setForeground(new Color(230, 167, 86));
+		passWord_user.setHorizontalAlignment(SwingConstants.CENTER);
+		passWord_user.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		passWord_user.setBounds(21, 248, 146, 14);
+		frame.getContentPane().add(passWord_user);
+
+		name = new JTextField();
+		name.setBounds(169, 163, 356, 33);
+		frame.getContentPane().add(name);
+		name.setColumns(10);
+
+		passWord = new JTextField();
+		passWord.setBounds(169, 241, 356, 33);
+		frame.getContentPane().add(passWord);
+		passWord.setColumns(10);
+
+		JButton btn_connection = new JButton("SE CONNECTER");
+		btn_connection.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btn_connection.setForeground(Color.WHITE);
+		btn_connection.setBackground(new Color(88, 55, 30));
+		btn_connection.setBounds(204, 306, 157, 33);
+		frame.getContentPane().add(btn_connection);
+
+	}
+}
