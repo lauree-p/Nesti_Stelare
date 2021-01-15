f54b90a6f3fdc7a72f243a0a1834a72a0c660368
ste <s.cadeck@gmail.com>
2021-01-15 09:34:29 +0100

    add view login

--- Nesti_Stelare/src/views/View_Login.java
+++ Nesti_Stelare/src/views/View_Login.java
@@ -49,44 +49,54 @@ public class View_Login {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setResizable(false);
 		frame.getContentPane().setBackground(new Color(46, 22, 14));
 		frame.getContentPane().setLayout(null);
-		frame.setBounds(100, 100, 575, 400);
+		frame.setBounds(100, 100, 496, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		/**
+		 * Nesti logo.
+		 */
 		JLabel img = new JLabel(new ImageIcon(View_Login.class.getResource("/img/connect-image.png")));
-		img.setBounds(42, 0, 478, 127);
+		img.setBounds(0, 0, 480, 127);
 		frame.getContentPane().add(img);
 
 		JLabel name_user = new JLabel("Nom utilisateur");
 		name_user.setForeground(new Color(230, 167, 86));
 		name_user.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		name_user.setHorizontalAlignment(SwingConstants.CENTER);
-		name_user.setBounds(21, 170, 157, 14);
+		name_user.setBounds(-12, 170, 157, 14);
 		frame.getContentPane().add(name_user);
 
 		JLabel passWord_user = new JLabel("Mot de passe");
 		passWord_user.setForeground(new Color(230, 167, 86));
 		passWord_user.setHorizontalAlignment(SwingConstants.CENTER);
 		passWord_user.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		passWord_user.setBounds(21, 248, 146, 14);
+		passWord_user.setBounds(-12, 248, 146, 14);
 		frame.getContentPane().add(passWord_user);
 
+		/**
+		 * Fields to fill for connection.
+		 */
 		name = new JTextField();
-		name.setBounds(169, 163, 356, 33);
+		name.setBounds(124, 163, 346, 33);
 		frame.getContentPane().add(name);
 		name.setColumns(10);
 
 		passWord = new JTextField();
-		passWord.setBounds(169, 241, 356, 33);
+		passWord.setBounds(124, 241, 346, 33);
 		frame.getContentPane().add(passWord);
 		passWord.setColumns(10);
 
+		/**
+		 * Button to connect.
+		 */
 		JButton btn_connection = new JButton("SE CONNECTER");
 		btn_connection.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btn_connection.setForeground(Color.WHITE);
 		btn_connection.setBackground(new Color(88, 55, 30));
-		btn_connection.setBounds(204, 306, 157, 33);
+		btn_connection.setBounds(171, 306, 157, 33);
 		frame.getContentPane().add(btn_connection);
 
 	}
