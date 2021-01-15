f155b008592e2bab0be70b58bcd49e7544d51e50
ste <s.cadeck@gmail.com>
2021-01-15 17:24:54 +0100

    update  view orders and products

--- Nesti_Stelare/src/views/View_Orders.java
+++ Nesti_Stelare/src/views/View_Orders.java
@@ -2,15 +2,26 @@ package views;
 
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class View_Orders {
-	
+
 	static JPanel panel_orders;
-	
+	private JTextField _Number_Product;
+	private JTable table;
+
 	public View_Orders() {
 		initialize();
 	}
@@ -19,42 +30,105 @@ public class View_Orders {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
+
 		// Data Arrays
-		String data_orders[][]={ 
-	    		{"02/02/21","06/02/21","Boite de 6 oeufs","10","en attente","Le Comptoir Paysans d'Oc","Stephane-cad55"}  
-                };    
-	    String[] columnNames_orders={"Date de la commande","Date de réception","Article","Quantité","Etat","Producteur","Administrateur",};
-	    
+		String data_orders[][] = { { "02/02/21", "06/02/21", "Boite de 6 oeufs", "10", "en attente",
+				"Le Comptoir Paysans d'Oc", "Stephane-cad55" } };
+		String[] columnNames_orders = { "Date de la commande", "Date de réception", "Article", "Quantité", "Etat",
+				"Producteur", "Administrateur", };
+
+		// Data Arrays
+		String data_orders_list_orders[][] = { { "Boite de 6 oeufs", "Ingrédient" },
+				{ "Paquet de farine", "Ingrédient" }, { "Lot de 2 fourchette", "Ustensile" } };
+		String[] columnNames_list_orders = { "Nom", "Type" };
+
 		// All Panels (TabbedPane)
 		panel_orders = new JPanel();
 		panel_orders.setBackground(Color.WHITE);
 		View_App.tabbedPane.addTab("Commandes", null, panel_orders, null);
 		panel_orders.setLayout(null);
-		
-	    // Création du ScrollPane
+
+		// Création du ScrollPane
 		JScrollPane scrollPane_orders = new JScrollPane();
 		scrollPane_orders.setBounds(0, 0, 888, 585);
-		
+
 		// Ajout du ScrollPane à panel_articles
 		panel_orders.add(scrollPane_orders);
-		
-		JTable table_orders = new JTable(data_orders,columnNames_orders);
-		
+
+		JTable table_orders = new JTable(data_orders, columnNames_orders);
+
 		scrollPane_orders.setViewportView(table_orders);
-        scrollPane_orders.setColumnHeaderView(table_orders.getTableHeader());
-        
-        JPanel panel = new JPanel();
-        panel.setBounds(898, 11, 328, 273);
-        panel.setBackground(Color.WHITE);
-        panel_orders.add(panel);
-        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 5));
-        
-        JPanel panel_1 = new JPanel();
-        panel_1.setBackground(Color.WHITE);
-        panel_1.setBounds(898, 295, 328, 279);
-        panel_orders.add(panel_1);
-        panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 5));
+		scrollPane_orders.setColumnHeaderView(table_orders.getTableHeader());
+
+		JPanel panel = new JPanel();
+		panel.setBounds(898, 11, 328, 214);
+		panel.setBackground(Color.WHITE);
+		panel_orders.add(panel);
+		panel.setLayout(null);
+
+		JLabel lbl_Orders = new JLabel("Ajouter un produit \u00E0 la commande");
+		lbl_Orders.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lbl_Orders.setBounds(62, 11, 220, 14);
+		lbl_Orders.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lbl_Orders);
+
+		JLabel lbl_Company_Name = new JLabel("S\u00E9lectionnez l'entreprise");
+		lbl_Company_Name.setBounds(10, 47, 148, 14);
+		panel.add(lbl_Company_Name);
+
+		JLabel lbl_Name_Product = new JLabel("Nom du produit :");
+		lbl_Name_Product.setBounds(10, 103, 176, 14);
+		panel.add(lbl_Name_Product);
+
+		JButton btn_Add_Orders = new JButton("Ajouter \u00E0 la commande");
+		btn_Add_Orders.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btn_Add_Orders.setBounds(79, 171, 191, 23);
+		panel.add(btn_Add_Orders);
+
+		JComboBox comboBox_Name_Product = new JComboBox();
+		comboBox_Name_Product.setBounds(10, 128, 176, 22);
+		panel.add(comboBox_Name_Product);
+
+		_Number_Product = new JTextField();
+		_Number_Product.setBounds(196, 129, 122, 20);
+		panel.add(_Number_Product);
+		_Number_Product.setColumns(10);
+
+		JLabel lbl_Number_Products = new JLabel("Nombre de produits :");
+		lbl_Number_Products.setBounds(196, 103, 191, 14);
+		panel.add(lbl_Number_Products);
+
+		JComboBox comboBox_Company = new JComboBox();
+		comboBox_Company.setBounds(10, 72, 308, 22);
+		panel.add(comboBox_Company);
+
+		JPanel panel_List_Orders = new JPanel();
+		panel_List_Orders.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_List_Orders.setBackground(Color.WHITE);
+		panel_List_Orders.setBounds(898, 295, 328, 279);
+		panel_orders.add(panel_List_Orders);
+		panel_List_Orders.setLayout(null);
+
+		JScrollPane scrollPane_List_Orders = new JScrollPane();
+		scrollPane_List_Orders.setBounds(0, 34, 328, 184);
+		panel_List_Orders.add(scrollPane_List_Orders);
+
+		JTable table_Articles_Orders = new JTable(data_orders_list_orders, columnNames_list_orders);
+		scrollPane_List_Orders.setColumnHeaderView(table_Articles_Orders);
+		scrollPane_List_Orders.setViewportView(table_Articles_Orders);
+
+		JLabel lbl_title_list_orders = new JLabel("Liste des produits de la commande");
+		lbl_title_list_orders.setBounds(0, 0, 328, 34);
+		lbl_title_list_orders.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lbl_title_list_orders.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_List_Orders.add(lbl_title_list_orders);
+
+		JButton btn_Add_This_Orders = new JButton("Ajouter cette commande");
+		btn_Add_This_Orders.setBounds(81, 234, 182, 23);
+		panel_List_Orders.add(btn_Add_This_Orders);
 
 	}
 }
--- Nesti_Stelare/src/views/View_Products.java
+++ Nesti_Stelare/src/views/View_Products.java
@@ -6,11 +6,16 @@ import java.awt.FlowLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
 
 public class View_Products {
-	
+
 	static JPanel panel_products;
-	
+
 	public View_Products() {
 		initialize();
 	}
@@ -19,45 +24,79 @@ public class View_Products {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
+
 		// Data Arrays
-	    String data_products[][]={ 
-	    		{"Boite de 6 oeufs","Ingrédient","2.50$","800g","boîte","actif","2"},    
-	    		{"Paquet de farine","Ingrédient","1.45$","430g","paquet","en attente","4"},
-	    		{"Lot de 2 fourchette","Ustensile","4.20$","100g","lot","bloqué","1"}
-                };    
-	    String[] columnNames_products={"Nom","Type","Prix","Poid","Conditionnement","Etat","Stock"};
-	    
-	    
+		String data_products[][] = { { "oeuf", "Ingrédient", "poussin2021" }, { "farine", "Ingrédient", "cadeck2021" },
+				{ "fourchette", "Ustensile", "duban2021" } };
+		String[] columnNames_products = { "Nom", "Type", "Administrateur qui a rentré le produit" };
+
 		// All Panels (TabbedPane)
 		panel_products = new JPanel();
 		panel_products.setBackground(Color.WHITE);
 		View_App.tabbedPane.addTab("Produits", null, panel_products, null);
 		panel_products.setLayout(null);
-		
-	    // Création du ScrollPane
+
+		// Création du ScrollPane
 		JScrollPane scrollPane_products = new JScrollPane();
-		scrollPane_products.setBounds(0, 0, 888, 585);
-		
+		scrollPane_products.setBounds(0, 0, 708, 585);
+
 		// Ajout du ScrollPane à panel_articles
 		panel_products.add(scrollPane_products);
-		
-		JTable table_products = new JTable(data_products,columnNames_products);
-		
+
+		JTable table_products = new JTable(data_products, columnNames_products);
+
 		scrollPane_products.setViewportView(table_products);
-        scrollPane_products.setColumnHeaderView(table_products.getTableHeader());
-        
-        JPanel panel = new JPanel();
-        panel.setBounds(898, 11, 328, 273);
-        panel.setBackground(Color.WHITE);
-        panel_products.add(panel);
-        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 5));
-        
-        JPanel panel_1 = new JPanel();
-        panel_1.setBackground(Color.WHITE);
-        panel_1.setBounds(898, 295, 328, 279);
-        panel_products.add(panel_1);
-        panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 5));
+		scrollPane_products.setColumnHeaderView(table_products.getTableHeader());
+
+		JPanel panel = new JPanel();
+		panel.setBounds(812, 11, 328, 247);
+		panel.setBackground(Color.WHITE);
+		panel_products.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lbl_Type = new JLabel("Type de produits :");
+		lbl_Type.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_Type.setBounds(0, 53, 129, 14);
+		panel.add(lbl_Type);
+
+		JLabel lbl_Products_List = new JLabel("Liste des produits");
+		lbl_Products_List.setBounds(111, 11, 110, 15);
+		lbl_Products_List.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lbl_Products_List.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lbl_Products_List);
+		
+		JComboBox comboBox_Type = new JComboBox();
+		comboBox_Type.setBounds(19, 78, 136, 22);
+		panel.add(comboBox_Type);
+		
+		JLabel lbl_Name_Products = new JLabel("Nom du produit : ");
+		lbl_Name_Products.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_Name_Products.setBounds(0, 121, 129, 14);
+		panel.add(lbl_Name_Products);
+		
+		JComboBox comboBox_Name = new JComboBox();
+		comboBox_Name.setBounds(19, 146, 136, 22);
+		panel.add(comboBox_Name);
+		
+		JButton btn_Add_Products = new JButton("Ajouter un produit");
+		btn_Add_Products.setBounds(98, 200, 145, 23);
+		panel.add(btn_Add_Products);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.WHITE);
+		panel_1.setBounds(812, 269, 328, 110);
+		panel_products.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JComboBox _Administrators = new JComboBox();
+		_Administrators.setBounds(33, 49, 122, 22);
+		panel_1.add(_Administrators);
+		
+		JLabel lbl_Administrators = new JLabel("Adminstrateurs");
+		lbl_Administrators.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lbl_Administrators.setBounds(100, 11, 128, 14);
+		lbl_Administrators.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_1.add(lbl_Administrators);
 
 	}
 }
