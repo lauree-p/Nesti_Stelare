2915fd6781a159154f1be5a4a2fe4feaacd682b2
Laurée <poussin.laurelenne@gmail.com>
2021-01-22 14:45:21 +0100

    upload View artiles encodage , Config.java, SuperAdmin.java

--- Nesti_Stelare/src/models/SuperAdmin.java
+++ Nesti_Stelare/src/models/SuperAdmin.java
@@ -1,12 +1,10 @@
 package models;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
 
 public class SuperAdmin extends Administrators {
 	public static void main(String[] args) {
-		addAdmin("stephane","torototo");
+		deleteAdmin(9);
 	}
 
 	public static void addAdmin(String nickName, String passWord) {
@@ -20,13 +18,13 @@ public class SuperAdmin extends Administrators {
 			System.err.println("erreur lors de la creation");
 		}
 	}
-	public static void updateAdmin(int id_admin, String nickName, String passWord) {
+	public static void updateAdmin(int id_admin, String nickName, String passWord, String state) {
 		
 		
 		try {
 			MyConnexion.openConnection();
 			java.sql.Statement declaration = MyConnexion.accessDataBase.createStatement();
-			String query = "UPDATE administrators SET nickName='"+nickName+"', passWord='"+passWord+"' WHERE Id_administrators="+id_admin+";";
+			String query = "UPDATE administrators SET nickName='"+nickName+"', passWord='"+passWord+"', state='"+state+"' WHERE Id_administrators="+id_admin+";";
 			declaration.executeUpdate(query);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -34,6 +32,7 @@ public class SuperAdmin extends Administrators {
 		}
 		
 	}
+	
 	public static void deleteAdmin(int id_admin) {
 		
 		
@@ -43,8 +42,15 @@ public class SuperAdmin extends Administrators {
 			String query = "DELETE FROM `administrators` WHERE `administrators`.`Id_administrators` = "+id_admin+";";
 			declaration.executeUpdate(query);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.err.println("erreur lors de la mise a jour");
+			try {
+				MyConnexion.openConnection();
+				java.sql.Statement declaration = MyConnexion.accessDataBase.createStatement();
+				String query = "UPDATE administrators SET state='b' WHERE Id_administrators="+id_admin+";";
+				declaration.executeUpdate(query);
+			} catch (SQLException f) {
+				System.err.println("erreur update impossible");
+			}
+			System.err.println("erreur pas d'amin correspondant");
 		}
 		
 	}
