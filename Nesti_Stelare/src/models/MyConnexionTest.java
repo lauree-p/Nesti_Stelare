56bdcffe70af9d311e6d19e176ebd4d209900a73
ste <s.cadeck@gmail.com>
2021-06-10 15:24:51 +0200

    Update testUnit

--- Nesti_Stelare/src/models/MyConnexionTest.java
+++ Nesti_Stelare/src/models/MyConnexionTest.java
@@ -10,22 +10,22 @@ class MyConnexionTest {
 
 	@Test
 	void testCheckUser() {
-		MyConnexion test = new MyConnexion();
+		
 		SuperAdmin.addAdmin("testUnitaire1", "testUnitaire");
 		SuperAdmin.addAdmin("testUnitaire2", "testUnitaire");
-		assertTrue(test.checkUser("testUnitaire1"));
-		assertFalse(test.checkUser("toto"));
+		assertTrue(MyConnexion.checkUser("testUnitaire1"));
+		assertFalse(MyConnexion.checkUser("toto"));
 		SuperAdmin.deleteAdmin("testUnitaire1");
 		SuperAdmin.deleteAdmin("testUnitaire2");
 	}
 	
 	@Test
 	void testCheckId() {
-		MyConnexion test = new MyConnexion();
+	
 		SuperAdmin.addAdmin("testUnitaire1", "testUnitaire");
 		SuperAdmin.addAdmin("testUnitaire2", "testUnitaire");
-		assertTrue(test.checkId("testUnitaire2", MD5.main("testUnitaire")));
-		assertFalse(test.checkId("toto", MD5.main("testUnitaire")));
+		assertTrue(MyConnexion.checkId("testUnitaire2", MD5.main("testUnitaire")));
+		assertFalse(MyConnexion.checkId("toto", MD5.main("testUnitaire")));
 		SuperAdmin.deleteAdmin("testUnitaire1");
 		SuperAdmin.deleteAdmin("testUnitaire2");
 	}
